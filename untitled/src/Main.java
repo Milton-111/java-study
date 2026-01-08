2d6f08677e083c91b2b9a602b38fd0403d0d68b5
tfh <3200511489@qq.com>
2026-01-08 12:24:54 +0800

    第一次提交

--- untitled/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-        System.out.print("hello 中国！！");
-        System.out.println("hello 中国！！");
-        System.out.println("hello 中国！！");
-    }
-}
